9ae462a17341c548868a9dbc7254dcb9e2333aa1
tursom <tursom@foxmail.com>
2026-05-22 12:56:22 +0800

    feat: add ErrorBoundary to catch and display React errors

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,16 +1,35 @@
-import { StrictMode } from 'react'
+import { StrictMode, Component } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import App from './App'
 
+class ErrorBoundary extends Component<{ children: React.ReactNode }, { error: Error | null }> {
+  state = { error: null as Error | null }
+  static getDerivedStateFromError(error: Error) { return { error } }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ padding: 48, color: 'red', fontFamily: 'monospace' }}>
+          <h1>应用错误</h1>
+          <pre>{this.state.error.message}</pre>
+          <pre style={{ fontSize: 12, marginTop: 16 }}>{this.state.error.stack}</pre>
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConfigProvider locale={zhCN}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ConfigProvider>
+    <ErrorBoundary>
+      <ConfigProvider locale={zhCN}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConfigProvider>
+    </ErrorBoundary>
   </StrictMode>,
 )
